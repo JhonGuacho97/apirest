26652526b6f99be7419e94979b45626517628f5c
Jhon Guacho <juniorbastidas1997@gmail.com>
2022-01-24 20:09:53 -0500

    segundo cambio

--- classes/server.ts
+++ classes/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export default class Server {
 
     public app: express.Application;
-    public port: number = 3000;
+    public port =  process.env.port ||  3000;
 
     constructor(){
         this.app = express();
