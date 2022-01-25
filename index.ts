4132adcf5af6d6da1f469cc59c766156133e2355
Jhon Guacho <juniorbastidas1997@gmail.com>
2022-01-25 00:59:53 -0500

    heroku

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ server.app.use('/post', postRoutes);
 
 
 
-mongoose.connect(process.env.MONGODB_CNN || 'mongodb://localhost:27017/loginserver',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/loginserver',
     (err)=>{
     if(err) throw err;
     console.log('base de datos online');
